2b050dd69340b90515b1d551cc006c25eaf66deb
elijah <yogurt777@protonmail.com>
2022-09-05 22:29:09 +0800

    [ADD] 添加第八章内容 函数 1、函数参数 2、导入模块 3、传递实参等

--- chapter8/greeter.py
+++ chapter8/greeter.py
@@ -3,9 +3,27 @@ def greet_user():
     print("Hello!")
 
 
-def favorite_book(name='bible'):
+def favorite_bvook(name='bible'):
     print("One of my favorite books is " + name.title())
 
+def get_formatted_name(first_name, last_name):
+    """返回整洁的姓名"""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
 
-favorite_book("Alice in Wonderland")
-favorite_book()
+# 这是一个无限循环！
+
+while True:
+    print("\nPlease tell me your name:")
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print("\nHello, " + formatted_name + "!")
+
+# favorite_book("Alice in Wonderland")
+# favorite_book()
